97df3f75aeef6160a80417e1405a650480521448
Dhruv Sharmaa <dhruvsharma22052006@gmai.com>
2026-04-01 21:47:40 +0530

    chore: update SearchBar component

--- weather-Dasboard /src/components/SearchBar.jsx
+++ weather-Dasboard /src/components/SearchBar.jsx
@@ -1,18 +1,13 @@
-export default function SearchBar({ cityInput, onCityInputChange, onSearch }) {
-  function onSubmit(event) {
-    event.preventDefault();
-    onSearch();
-  }
+import { useEffect } from "react";
 
-  return (
-    <form className="search-box" onSubmit={onSubmit}>
-      <input
-        type="text"
-        placeholder="Enter city name..."
-        value={cityInput}
-        onChange={(event) => onCityInputChange(event.target.value)}
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
+export default function SearchBar({ onSearch }) {
+
+  useEffect(() => {
+    onSearch("Pune");
+  }, []);
+
+  return null; 
 }
+
+
+
